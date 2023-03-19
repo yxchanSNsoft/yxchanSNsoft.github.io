b94f950fedc094498727041c94a5c5ac0ccb6b7d
yxchanb2b <46280186+yxchanb2b@users.noreply.github.com>
2023-03-19 23:23:37 +0800

    commit

--- _next/static/So9o6iUg3kkAf-uom31Oh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/737-bdf797c6d23cdf26.js","static/css/bff950a31bfd5d70.css","static/chunks/pages/index-9bdc17e00643b49d.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
