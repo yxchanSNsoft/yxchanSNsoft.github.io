05349917444811c189595cac3f2f40c60b3422d9
gnomemage1990@gmail.com <gnomemage1990@gmail.com>
2025-09-28 20:03:34 +0800

    update

---
+++ _next/static/wmx0eRdrBid4gfa99nbTW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/737-bdf797c6d23cdf26.js","static/css/686e2fdd90640fbd.css","static/chunks/pages/index-9dfbf10627f4f9c2.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
